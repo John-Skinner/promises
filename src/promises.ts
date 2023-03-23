d3cc03ce4ca857c2aa11c7c5013f0cec98fb268f
john-skinner <j.v.skinner.jr@gmail.com>
2023-03-23 14:31:36 -0500

    nested with a chain

--- src/promises.ts
+++ src/promises.ts
@@ -1,16 +1,34 @@
 export class PromiseChain {
     getNestedPromises(): Promise<string> {
         return new Promise<string>((res, rej) => {
-            setTimeout(() => {
-                console.log('outer timeout end');
-                let p = new Promise<string>((resInner, rejInner) => {
-                    setTimeout(() => {
-                        console.log('inner timeout end');
-                        resInner('inner complete');
-                    }, 5000)
-                })
-                res(p);
-            }, 5000);
+            let p1 = this.getPromisedTimer('promiseTimer1');
+            p1.then((innerRes) => {
+                console.log('completed p1:' + innerRes);
+                let p2 = this.getPromisedTimer('promiseTimer2');
+                // return a next promise for chaining.
+                return p2;
+            }).then((innerRes2) => {
+                console.log('completed p2:' + innerRes2);
+                let p3 = this.getPromisedTimer('promiseTimer3');
+                // return yet another promise for chaining
+                return p3;
+
+            }).then((finalRes) => {
+                console.log('last inner.  let outer promise know.');
+                res('ok');
+            })
+
         })
     }
+
+    getPromisedTimer(message): Promise<any> {
+        let p = new Promise<any>((pRes, pRej) => {
+            setTimeout(() => {
+                console.log('finished:' + message);
+                pRes('resolved' + message);
+            }, 10000);
+        });
+        return p;
+    }
+
 }
